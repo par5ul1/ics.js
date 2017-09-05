b0902c7fd67292d51a6dd7cd184863439b8fa5ab
Parsuli <par5ul1@users.noreply.github.com>
2017-09-05 23:23:13 +0200

    Update ics.js

--- ics.js
+++ ics.js
@@ -111,17 +111,7 @@ var ics = function() {
             ext = (typeof ext !== 'undefined') ? ext : '.ics';
             filename = (typeof filename !== 'undefined') ? filename : 'calendar';
             var calendar = calendarStart + SEPARATOR + calendarEvents.join(SEPARATOR) + calendarEnd;
-
-            var blob;
-            if (navigator.userAgent.indexOf('MSIE 10') === -1) { // chrome or firefox
-                blob = new Blob([calendar]);
-            } else { // ie
-                var bb = new BlobBuilder();
-                bb.append(calendar);
-                blob = bb.getBlob('text/x-vCalendar;charset=' + document.characterSet);
-            }
-            saveAs(blob, filename + ext);
-            return calendar;
+            window.open( "data:text/calendar;charset=utf8," + escape(calendar));
         }
     };
 };
